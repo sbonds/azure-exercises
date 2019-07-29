44c5259f852e37589013e2a72cc936808c6056b5
Steve Bonds <sbonds@gmail.com>
2019-07-29 10:44:35 -0700

    Get $HOME Ruby-style works

--- local-tests/az-creds/controls/az-credentials.rb
+++ local-tests/az-creds/controls/az-credentials.rb
@@ -3,10 +3,6 @@
 title "Check Azure local credentials"
 
 # Checks on the credentials file
-# TODO: $HOME doesn't work either
-# http://www.anniehedgie.com/inspec-basics-10 suggests creating a Rakefile and using template processing to substitute
-# environment variables (e.g. $HOME) into Inspec attribiutes. Seems painful for a tool that's supposed to make things
-# easier.
-describe file("~/.azure/credentials") do
+describe file("#{ENV['HOME']}/.azure/credentials") do
   it { should exist }
 end
